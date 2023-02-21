683b80428e4ebe436177e5f282ecc26da3cfa50e
jaeyeong815 <jae0ee@naver.com>
2023-02-21 21:03:39 +0900

    [FEAT] tailwind CSS 초기 설정(#1)

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/pages/**/*.{ts,tsx}'],
+  theme: {
+    colors: {
+      transparent: 'transparent',
+      lightRed: '#ff7b54',
+      lightOrange: '#ffb26b',
+      yellow: '#ffd56f',
+      oliveGreen: '#939b62',
+      gray: '#565656',
+    },
+    extend: {},
+  },
+  plugins: [],
+};
