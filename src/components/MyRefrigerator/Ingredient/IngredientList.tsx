5cbaff1d93aa727ddde6e5e392099a87c3e322e6
jaeyeong815 <jae0ee@naver.com>
2023-02-27 16:22:27 +0900

    [FEAT] 재료 list 스타일 & 재료 선택 버튼 제거(#13)
    재료 선택 시 선택 버튼을 클릭했어야 했는데, 버튼을 삭제하고 재료 이름을 클릭하면 선택 되는 것으로 변경

--- src/components/MyRefrigerator/Ingredient/IngredientList.tsx
+++ src/components/MyRefrigerator/Ingredient/IngredientList.tsx
@@ -11,14 +11,13 @@ const IngredientList = () => {
   const handleDelete = (e: React.MouseEvent<HTMLElement>) => dispatch(deleteListInItem(e.currentTarget.id));
 
   return (
-    <ul className="m-8 w-60">
+    <ul className="my-20">
       {ingredientList.map((ingredient) => (
-        <li key={ingredient}>
-          <span className="inline-block w-40 p-0">{ingredient}</span>
-          <button id={ingredient} onClick={handleSelect}>
-            선택
-          </button>
-          <button id={ingredient} onClick={handleDelete}>
+        <li className="mb-3 flex items-center" key={ingredient}>
+          <span id={ingredient} onClick={handleSelect} className="item-ingredient">
+            {ingredient}
+          </span>
+          <button className="btn-option-del" id={ingredient} onClick={handleDelete}>
             삭제
           </button>
         </li>
