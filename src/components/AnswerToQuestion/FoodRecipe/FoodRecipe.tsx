e3f11bf9d573a7e337db358c4493c019a2ddc1a6
jaeyeong815 <jae0ee@naver.com>
2023-02-28 22:31:25 +0900

    [FEAT] 레시피 안내 페이지 디자인(#13)

--- src/components/AnswerToQuestion/FoodRecipe/FoodRecipe.tsx
+++ src/components/AnswerToQuestion/FoodRecipe/FoodRecipe.tsx
@@ -6,9 +6,32 @@ const FoodRecipe = () => {
 
   return (
     <>
-      {recipe?.map((el, idx) => (
-        <p key={idx}>{el + '\n'}</p>
-      ))}
+      {recipe.length > 0 && (
+        <div className="grid grid-cols-2 mx-10 md:mx-48">
+          <div className="justify-self-center">
+            <p className="font-extrabold text-center">재료</p>
+            {recipe.map((el) =>
+              el.includes('-') ? (
+                <div key={el}>
+                  <br />
+                  <p>{el.replace('-', '☑︎') + '\n'}</p>
+                </div>
+              ) : null
+            )}
+          </div>
+          <div className="justify-self-center">
+            <p className="font-extrabold text-center">레시피</p>
+            {recipe.map((el) =>
+              el.includes('.') ? (
+                <div key={el}>
+                  <br />
+                  <p>{el + '\n'}</p>
+                </div>
+              ) : null
+            )}
+          </div>
+        </div>
+      )}
     </>
   );
 };
