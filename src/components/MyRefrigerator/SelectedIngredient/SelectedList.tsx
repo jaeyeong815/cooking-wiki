da466f2fb40e4a54a28ac4e9afa31768622b523a
jaeyeong815 <jae0ee@naver.com>
2023-02-28 14:10:37 +0900

    [FEAT] 선택된 재료 list 스타일(#13)

--- src/components/MyRefrigerator/SelectedIngredient/SelectedList.tsx
+++ src/components/MyRefrigerator/SelectedIngredient/SelectedList.tsx
@@ -9,16 +9,20 @@ const SelectIngredientList = () => {
   const handleDelete = (e: React.MouseEvent<HTMLElement>) => dispatch(deleteSelectListInItem(e.currentTarget.id));
 
   return (
-    <ul>
-      {selectList.map((ingredient) => (
-        <li key={ingredient}>
-          <span className="inline-block w-40 p-0">{ingredient}</span>
-          <button id={ingredient} onClick={handleDelete}>
-            삭제
-          </button>
-        </li>
-      ))}
-    </ul>
+    <>
+      <hr className="border w-5/6 border-dashed " />
+      <p className="mt-20 font-bold text-xl">선택된 재료</p>
+      <ul className="my-6">
+        {selectList.map((ingredient) => (
+          <li className="mb-3 flex items-center" key={ingredient}>
+            <span className="inline-block item-ingredient">{ingredient}</span>
+            <button className="btn-option-del" id={ingredient} onClick={handleDelete}>
+              삭제
+            </button>
+          </li>
+        ))}
+      </ul>
+    </>
   );
 };
 
