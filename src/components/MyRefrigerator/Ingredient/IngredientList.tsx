28c448b1cafc568bc906a837a2f61de96888c68f
jaeyeong815 <jae0ee@naver.com>
2023-02-28 17:13:49 +0900

    [FEAT] 재료 미선택 시 선택해달라고 안내(#13)

--- src/components/MyRefrigerator/Ingredient/IngredientList.tsx
+++ src/components/MyRefrigerator/Ingredient/IngredientList.tsx
@@ -4,6 +4,7 @@ import { deleteListInItem, selectItem } from '@/store/slice/ingredientListSlice'
 
 const IngredientList = () => {
   const ingredientList = useSelector((state: RootState) => state.ingredientList.ingredientList);
+  const selectList = useSelector((state: RootState) => state.ingredientList.selectList);
   const dispatch = useDispatch();
 
   const handleSelect = (e: React.MouseEvent<HTMLElement>) => dispatch(selectItem(e.currentTarget.id));
@@ -11,24 +12,32 @@ const IngredientList = () => {
   const handleDelete = (e: React.MouseEvent<HTMLElement>) => dispatch(deleteListInItem(e.currentTarget.id));
 
   return (
-    <ul className="my-20">
-      {ingredientList.length === 0 && (
-        <div className="flex items-center">
-          <span className="text-xl">재료가 텅 비어있어요</span>
-          <img src="/sad.svg" className="w-12 h-12 inline-block" />
+    <>
+      <ul className="my-20">
+        {ingredientList.length === 0 && (
+          <div className="flex items-center">
+            <span className="text-xl">재료가 텅 비어있어요</span>
+            <img src="/sad.svg" className="w-12 h-12 inline-block" />
+          </div>
+        )}
+        {ingredientList.map((ingredient) => (
+          <li className="mb-3 flex items-center" key={ingredient}>
+            <span id={ingredient} onClick={handleSelect} className="item-ingredient">
+              {ingredient}
+            </span>
+            <button className="btn-option-del" id={ingredient} onClick={handleDelete}>
+              삭제
+            </button>
+          </li>
+        ))}
+      </ul>
+      {ingredientList.length > 0 && selectList.length === 0 && (
+        <div className="text-lg flex items-center">
+          <span>재료를 선택해주시면 요리를 추천해드릴게요!</span>
+          <img className="w-12 h-12 inline-block" src="/search.svg" />
         </div>
       )}
-      {ingredientList.map((ingredient) => (
-        <li className="mb-3 flex items-center" key={ingredient}>
-          <span id={ingredient} onClick={handleSelect} className="item-ingredient">
-            {ingredient}
-          </span>
-          <button className="btn-option-del" id={ingredient} onClick={handleDelete}>
-            삭제
-          </button>
-        </li>
-      ))}
-    </ul>
+    </>
   );
 };
 
