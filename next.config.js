9024a7f431a09bb4bd058fdcb77cf7f51478401d
jaeyeong815 <jae0ee@naver.com>
2023-03-04 20:10:18 +0900

    [CHORE] 배포를 위한 package 수정

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
+
+const debug = process.env.NODE_ENV !== 'production';
+const repository = 'cooking-wiki';
+
 const nextConfig = {
   reactStrictMode: false,
+  assetPrefix: !debug ? `/${repository}/` : '',
+  trailingSlash: true,
 };
 
 module.exports = nextConfig;
---
+++ src/utils/prefix.ts
@@ -0,0 +1,1 @@
+export const prefix = process.env.NODE_ENV === 'production' ? 'http://Storage-with-chatGPT.github.io/cooking-wiki' : '';
