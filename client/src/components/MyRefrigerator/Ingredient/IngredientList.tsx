61f5930f904ff76e6e328db0062ef36eb6b7fc22
jaeyeong815 <jae0ee@naver.com>
2023-03-22 16:24:26 +0900

    [REFACTOR] mock데이터 안내

--- client/src/components/MyRefrigerator/Ingredient/IngredientList.tsx
+++ client/src/components/MyRefrigerator/Ingredient/IngredientList.tsx
@@ -20,9 +20,17 @@ const IngredientList = () => {
     <>
       <ul className="my-10 overflow-y-auto h-64 px-10">
         {!hasIngredientList && (
-          <div className="flex items-center">
-            <span className="text-xl">재료가 텅 비어있어요</span>
-            <img src="/sad.svg" className="w-12 h-12 inline-block" />
+          <div className="flex flex-col justify-center items-center">
+            <div className="flex items-center">
+              <span className="text-xl">재료가 텅 비어있어요</span>
+              <img src="/sad.svg" className="w-12 h-12 inline-block" />
+            </div>
+            <div className="w-70 shadow-xl mt-8 py-3 flex flex-col items-center rounded-md bg-yellow/10">
+              <p className="font-bold text-xl">!필독!</p>
+              <p className="mx-5">현재 보고계시는 페이지에는 mock 데이터가 연결되어있습니다.</p>
+              <p className="mx-5">재료 입력, 선택은 가능하지만 추천 요리와 요리 레시피가 고정되어있습니다.</p>
+              <p className="mx-5">이 점 참고하고 테스트 해주시면 감사하겠습니다 !</p>
+            </div>
           </div>
         )}
         {ingredientList.map((ingredient) => (
